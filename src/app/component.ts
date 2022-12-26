638befff8ea41fe792ae027ff6ade6f51839245f
Dannie Myers <dmyers19@woh.rr.com>
2022-12-25 19:14:14 -0500

    Class Binding

--- src/app/component.ts
+++ src/app/component.ts
@@ -6,7 +6,10 @@ import { Model } from "./repository.model";
 })
 export class ProductComponent {
     model: Model = new Model();
-    getClasses(): string {
-        return this.model.getProducts().length == 5 ? "bg-success" : "bg-warning";
+    
+    getClasses(key: number): string {
+        let product = this.model.getProduct(key);
+        console.log("component.ts - getClasses called. product price is " + product.price);
+        return "p-2 " + (product.price < 50 ? "bg-info" : "bg-warning");
     }
 }
