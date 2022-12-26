3382418e2463f57f28399350660bbe7a1f1b4101
Dannie Myers <dmyers19@woh.rr.com>
2022-12-26 15:55:14 -0500

    [ngClass] Changes

--- src/app/component.ts
+++ src/app/component.ts
@@ -1,11 +1,29 @@
-import { Component } from "@angular/core";
+import { ApplicationRef, Component } from "@angular/core";
 import { Model } from "./repository.model";
+import { Product } from "./product.model";
+
 @Component({
     selector: "app",
     templateUrl: "template.html"
 })
 export class ProductComponent {
     model: Model = new Model();
+
+    constructor(ref: ApplicationRef) {
+        (<any>window).appRef = ref;
+        (<any>window).model = this.model;
+        console.log("ProductComponent constructor called");
+        console.log("ref value: ");
+    }
+    
+    getProductByPosition(position: number): Product {
+        return this.model.getProducts()[position];
+    }
+    
+    getClassesByPosition(position: number): string {
+        let product = this.getProductByPosition(position);
+        return "p-2 " + (product.price < 50 ? "bg-info" : "bg-warning");
+    }
     
     getClasses(key: number): string {
         let product = this.model.getProduct(key);
